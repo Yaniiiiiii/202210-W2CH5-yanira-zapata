81144ede078985c37389a3715c5793d99df3db2c
Yanira <yanirazapata.c@gmail.com>
2022-10-15 18:08:51 +0200

    change-cell-status

--- modules/functions.js
+++ modules/functions.js
@@ -1,7 +1,20 @@
-import {
-    gameOfLife,
-    countingAliveCells,
-    countingDeathCells,
-} from './modules/counter.js';
+import { gameOfLife } from './board.js';
+import { countingAliveCells, countingDeathCells } from './counter.js';
 
-const changingCellStatus = () => {};
+export const changingCellStatus = (board) => {
+    const dataDeathItems = countingDeathCells(board);
+    const dataAliveItems = countingAliveCells(board);
+    const board2 = board;
+    dataDeathItems.forEach((deathItem) => {
+        if (deathItem.countN === 3) {
+            board2[deathItem.r][deathItem.c] = 1;
+        }
+    });
+    dataAliveItems.forEach((aliveItem) => {
+        if (aliveItem.countN < 2) {
+            board2[aliveItem.r][aliveItem.c] = 0;
+        }
+    });
+    return board;
+};
+console.log(changingCellStatus(gameOfLife));
