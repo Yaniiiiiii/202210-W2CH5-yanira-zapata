2f4e73fb62fe95043e2e5a9484033a9d1eb6273e
Yanira <yanirazapata.c@gmail.com>
2022-10-15 19:19:51 +0200

    add sonar cloud fixes

--- modules/counter.js
+++ modules/counter.js
@@ -1,9 +1,4 @@
-export function AliveItems(r, c, countN) {
-    this.r = r;
-    this.c = c;
-    this.countN = countN;
-}
-export function DeathItems(r, c, countN) {
+export function Cell(r, c, countN) {
     this.r = r;
     this.c = c;
     this.countN = countN;
@@ -11,8 +6,8 @@ export function DeathItems(r, c, countN) {
 
 export const countingAliveCells = (array) => {
     let arrayAliveObjects = [];
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length; j++) {
+    for (let i = 1; i < array.length - 1; i++) {
+        for (let j = 1; j < array.length - 1; j++) {
             if (array[i][j] === 1) {
                 let count = 0;
                 if (array[i - 1][j] === 1) count++;
@@ -24,7 +19,7 @@ export const countingAliveCells = (array) => {
                 if (array[i + 1][j - 1] === 1) count++;
                 if (array[i - 1][j + 1] === 1) count++;
                 if (array[i + 1][j + 1] === 1) count++;
-                arrayAliveObjects.push(new AliveItems(i, j, count));
+                arrayAliveObjects.push(new Cell(i, j, count));
             }
         }
     }
@@ -45,7 +40,7 @@ export const countingDeathCells = (array) => {
                 if (array[i + 1][j - 1] === 1) count++;
                 if (array[i - 1][j + 1] === 1) count++;
                 if (array[i + 1][j + 1] === 1) count++;
-                arrayDeathObjects.push(new DeathItems(i, j, count));
+                arrayDeathObjects.push(new Cell(i, j, count));
             }
         }
     }
