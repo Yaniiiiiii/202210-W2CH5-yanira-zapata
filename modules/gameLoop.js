32a4ca34bb7167f2b6a017e0acfb1281366acefd
Yanira <yanirazapata.c@gmail.com>
2022-10-16 15:35:27 +0200

    refactoring gameLoop function

--- modules/gameLoop.js
+++ modules/gameLoop.js
@@ -1,10 +1,6 @@
 import { changingCellStatus } from './changeCellStatus.js';
-import { gameOfCells } from './board.js';
 
 export const changeArrayContent = (board) => {
-    for (let i = 0; i < 1; i++) {
-        console.table(board);
-        board = changingCellStatus(board);
-        continue;
-    }
+    console.table(board);
+    board = changingCellStatus(board);
 };
